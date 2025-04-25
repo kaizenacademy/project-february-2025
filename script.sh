bae275012b6410b246d270590e75501a7b8a9129
kaizenacademy <kaizen.techacademy@gmail.com>
2025-04-25 00:12:31 +0000

    new

--- script.sh
+++ script.sh
@@ -21,6 +21,11 @@ function update_ip() {
     terraform output -raw ec2 > ../ansible/hosts
 }
 
+function ansible() {
+    cd ../ansible
+    ansible-playbook main.yml
+}
+
 prepare_bastion
 create_instance
 update_ip
